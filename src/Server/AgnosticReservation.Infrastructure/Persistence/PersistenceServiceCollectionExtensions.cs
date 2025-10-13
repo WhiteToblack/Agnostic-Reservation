f9c3b63fb951e1833ae194477c54302fd79f787d
WhiteToblack <40027720+WhiteToblack@users.noreply.github.com>
2025-10-14 01:01:55 +0300

    Fix infrastructure dependencies

--- src/Server/AgnosticReservation.Infrastructure/Persistence/PersistenceServiceCollectionExtensions.cs
+++ src/Server/AgnosticReservation.Infrastructure/Persistence/PersistenceServiceCollectionExtensions.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace AgnosticReservation.Infrastructure.Persistence;
 
