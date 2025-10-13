d60447587c88791053b2a65d8be6d3979e6d8785
WhiteToblack <mert-cengiz@hotmail.com>
2025-10-14 01:18:17 +0300

    asd

--- src/Server/AgnosticReservation.Application/obj/Debug/net8.0/AgnosticReservation.Application.AssemblyInfo.cs
+++ src/Server/AgnosticReservation.Application/obj/Debug/net8.0/AgnosticReservation.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgnosticReservation.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ed0163ce884253e3c0008a79c2034fb2de61a58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8012c371f0eb4d67afbbbbcd497e2960d63cc9d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgnosticReservation.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgnosticReservation.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
